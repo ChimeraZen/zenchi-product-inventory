ef0ffb015191d038c38ae6ee9ed8a9138acb700b
Elijah Liedtke <chimera.zen@gmail.com>
2019-01-31 16:08:00 -0500

    Minor correction

--- backend/config/connection.js
+++
@@ -1,7 +0,0 @@
-// backend/config/connection.js
-const username = 'zenchi-inventory-demo-admin'
-const password = '2fXo66YtMWlJ6btu'
-const hostname = 'cluster0-faakr.mongodb.net'
-const database = 'zenchi-inventory-demo'
-
-module.exports.connect = `mongodb+srv://${username}:${password}@${hostname}/${database}`
