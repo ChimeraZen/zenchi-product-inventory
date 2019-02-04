4b0c4351d9030a3746b62a57295323fd4c5d4c1d
Elijah Liedtke <chimera.zen@gmail.com>
2019-02-04 13:32:37 -0500

    Removed modalReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,14 +3,12 @@ import { combineReducers } from 'redux'
 import errorReducer from './error'
 import authReducer from './auth'
 
-import modalReducer from './modal'
 //import productReducer from './product'
 import userReducer from './user'
 
 export default combineReducers({
   errors: errorReducer,
   auth: authReducer,
-  modal: modalReducer,
   //inventory: productReducer,
   userContext: userReducer,
 })
